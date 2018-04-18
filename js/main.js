527e5f43bac86cbe014d4bf2b94f7d60f21c1d01
AlexCSS <kalyuzhnyi@gmail.com>
2018-04-18 15:00:48 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,22 @@
+;(function () {
+	"use strict";
+
+	// Add item to incompleted-list
+		 // Creat task li element from template
+		
+		//Insert task value from the form
+
+		//Clear and focus new task field
+
+
+	// Move item to completed-list
+		// If checkek move task to completed list
+
+
+	// Delete item
+	
+	 
+	// Edit item
+
+
+})();
